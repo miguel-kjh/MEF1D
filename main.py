521b7e84e5708dc5abded8a8314024b83d29df61
miguel-kjh <miguelangel.kjh@gmail.com>
2021-05-11 14:11:40 +0100

    added spanish comments

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ elements = 3
 #k = 205
 function = lambda x: 1 - pow(x,2) #np.sin(np.pi*x) #1 - pow(x,2) #np.exp(-k*(x-5)**2)
 solution = None#lambda x: np.sin(np.pi*x)/np.pi**2
-n_cond = [None, None]
-d_cond = [0, 1] #[0, 0]
+n_cond = [1, None]
+d_cond = [None, 0] #[0, 0]
 interval = [0, 1] #[0, 10]
 
 if __name__ == '__main__':
     m    = Mesh(elements, a=interval[0], b=interval[1])
     m_g  = SolverMef1D(m, function, neumann_cond=n_cond, dirichlet_cond=d_cond)
-    view = SolutionView(100,a=interval[0], b=interval[1])
+    view = SolutionView(40,a=interval[0], b=interval[1])
     view.view(m_g, solution=solution)
 
